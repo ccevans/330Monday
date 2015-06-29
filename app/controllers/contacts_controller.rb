621a7ada3faec5faf763a1fcc3358cafeea0ddb4
CC Evans <ccevans1@gmail.com>
2015-06-29 15:04:15 -0400

    testing out email

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,5 @@
 class ContactsController < ApplicationController
-  respond_to :html, :json, :js
+
 
   def new
     @contact = Contact.new
@@ -9,11 +9,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      respond_to do |format|
-        format.html {redirect_to 'create'}
-        format.json
-        format.js
-      end
+  
       flash.now[:error] = nil
       flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
     else
