be085a900fc9edabf7e031f7a60ddec6362bb23d
CC Evans <ccevans1@gmail.com>
2015-06-29 13:46:45 -0400

    Added sendgrid production configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,19 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'monday330.herokuapp.com'}
 
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
+
   # config/environments/production.rb
 config.paperclip_defaults = {
   :storage => :s3,
